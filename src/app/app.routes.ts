c3c61627c06a6b4c0106e97004e630a4773fd2de
Alexandros aristeridis@hotmail.gr <aristeridis@hotmail.gr>
2025-10-12 18:21:49 +0300

    minor fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { UserHistoryComponent } from './components/user-history.components';
 
 export const routes: Routes = [
-    { path: '', component: UserHistoryComponent }
+    { path: '', redirectTo: '/history', pathMatch: 'full' },
+    { path: 'history', component: UserHistoryComponent },
 ];
